8be06668ad792972793ce0a1e6683c267afb2e9e
AndresaCA <avelarav@gmail.com>
2022-10-22 14:15:15 -0300

    add localStorage

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
-let id = 0 // id setada como nula de inicio
-
 // Forma reduzida usando template string
-const tarefa = (id) => `<div id='${id}'>
-<p>Tarefa<p/>
-<input type= "checkbox"/>
+let id = 0
+
+const tarefa = (id, novaTarefa) => `<div id='${id}'>
+<input type="checkbox" />
+<p>${novaTarefa}</p>
 </div>`
 
-const tarefaCriada = document.getElementById('NewTask')
-
-function addTarefa () {
-    id ++ // vai adicionando mais ids
-    document.querySelector('#listaTarefas').innerHTML += tarefa(id) //seleciona a div com id "listaTarefas" e adiciona como filha de tarefa (idTarefa)
+function exibirLista() {
+    id++
+    const tarefas = JSON.parse(localStorage.getItem('lista-tarefas'))
+    if(tarefas){
+        tarefas.forEach(tarefaListada => {
+            document.querySelector('#lista-tarefas').innerHTML += tarefa(id, tarefaListada)
+        })
+    }
 }
 
+function adicionarTarefa(){
+    id++
+    const novaTarefa = document.getElementById('nome-tarefa').value
+    document.querySelector('#lista-tarefas').innerHTML += tarefa(id, novaTarefa)
+    const listaTarefas = localStorage.getItem('lista-tarefas')
+    if(listaTarefas){
+        const novaLista = JSON.parse(listaTarefas)
+        novaLista.push(novaTarefa)
+        localStorage.setItem('lista-tarefas', JSON.stringify(novaLista))
+    } else {
+        localStorage.setItem('lista-tarefas', JSON.stringify([novaTarefa]))
+    }
+}
+
+exibirLista()
+
 // Forma extensiva
-/*function addTarefa () {
+/* function addTarefa () {
     id ++ // vai adicionando mais ids
 
     const tarefa = document.createElement('div') // cria um elemento div
     const nomeTarefa = document.createElement('p') // cria um elemento p
     const checkbox = document.createElement('input') // cria um elemento input
-
     nomeTarefa.innerHTML = 'Tarefa' // Vai escrever o texto tarefa
     checkbox.type = 'checkbox' // define o input checkbox como checkbox
     tarefa.id = id //cria um id para o elemento div
-
+    nomeTarefa.innerHTML = newTask.value
     tarefa.appendChild(nomeTarefa) // adiciona o paragrafo com a id nomeTarefa na div tarefa
     tarefa.appendChild(checkbox) //adiciona o checkbox como filha de tarefa 
     document.querySelector('#listaTarefas').appendChild(tarefa) //seleciona a div com id "listaTarefas" e adiciona como filha de tarefa (idTarefa)
