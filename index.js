6909731f9357adbc7c2830a95d59ca6edafce2f0
AndresaCA <avelarav@gmail.com>
2022-11-05 14:46:14 -0300

    add tema

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-let id = 0
+/* Aplicar tema*/
+const corTemas = document.querySelectorAll('[name="tema"]')
 
+// Guarda o tema no localStorage
+const guardarTema = function(tema) {
+    localStorage.setItem('tema', tema)
+}
+
+const retirarTema = function() {
+    const temaAtual = localStorage.getItem('tema')
+    corTemas.forEach((opçaoTema) => {
+        if (opçaoTema.id === temaAtual) {
+            opçaoTema.checked = true
+        }
+    })
+}
+
+corTemas.forEach(opçaoTema => {
+    opçaoTema.addEventListener('click', () => {
+        guardarTema(opçaoTema.id)
+    })
+})
+
+document.onload = retirarTema()
+
+/* Adicionar e remover tarefas*/
+let id = 0
 const tarefa = (id, novaTarefa) => `<div>
 <span>
 <input type="checkbox" onchange="marcarTarefa(${id})"/>
@@ -36,10 +61,9 @@ const validarTarefa = (novaTarefa) => {
         listaTarefas.map(tarefa => {
             if(tarefa === novaTarefa){
                 tarefaExistente = true
-                alert('Tarefa já existente')
+                alert('Essa tarefa já existe.')
             }
         })
-    
         return tarefaExistente
     }
 }
@@ -51,15 +75,32 @@ function adicionarTarefa(){
     if(validarTarefa(novaTarefa)){
         return
     }
-
     document.querySelector('#lista-tarefas').innerHTML += tarefa(id, novaTarefa)
-
     if(listaTarefas){
         const novaLista = JSON.parse(listaTarefas)
         novaLista.push(novaTarefa)
         localStorage.setItem('lista-tarefas', JSON.stringify(novaLista))
     } else {
         localStorage.setItem('lista-tarefas', JSON.stringify([novaTarefa]))
+    }
+}
+
+function adicionarTarefaEnter(event) {
+    if (event.keyCode == 13) {
+        id++
+    const novaTarefa = document.getElementById('nome-tarefa').value
+    const listaTarefas = localStorage.getItem('lista-tarefas')
+    if(validarTarefa(novaTarefa)){
+        return
+    }
+    document.querySelector('#lista-tarefas').innerHTML += tarefa(id, novaTarefa)
+    if(listaTarefas){
+        const novaLista = JSON.parse(listaTarefas)
+        novaLista.push(novaTarefa)
+        localStorage.setItem('lista-tarefas', JSON.stringify(novaLista))
+    } else {
+        localStorage.setItem('lista-tarefas', JSON.stringify([novaTarefa]))
+    }
     }
 }
 
@@ -71,5 +112,4 @@ const removerTarefa = (id) => {
     document.querySelector('#lista-tarefas').innerHTML = ''
     exibirLista()
 }
-
 exibirLista()
